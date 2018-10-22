36e2bf507ec139801730bbb2a6a2dcf28f9dd206
StephenHakami <stephen@sales-torch.com>
2018-10-22 11:45:16 -0400

    sign up and log in

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+
+helper_method :current_user, :is_logged_in?
+
+before_action :current_user
+
+  def current_user
+    if is_logged_in?
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+      @current_user
+    end
+
+
+    def is_logged_in?
+      session[:user_id].present?
+    end
+
+end
